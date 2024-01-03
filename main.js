e3a26669686891cc9a9b65566de9c648374e6815
Naya252 <naya252naya@gmail.com>
2024-01-03 20:42:35 +0300

    feat: add functions to create DOM

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 import './src/sass/style.module.scss';
+import createGallowsSection from './src/ui/layouts/gallows-section';
+import { createQuizSection, creteHintContent, creteWord } from './src/ui/layouts/quiz-section';
+import { changeTitle } from './src/shared/helpers';
+
+const app = document.createElement('div');
+app.className = 'container';
+
+const gallowSection = createGallowsSection();
+const quizSection = createQuizSection();
+app.append(gallowSection);
+app.append(quizSection);
+
+setTimeout(() => {
+  creteHintContent('Hint: Some text');
+  creteWord(['w', 'o', 'r', 'd']);
+}, 100);
+
+document.querySelector('.body').append(app);
+
+let size = window.innerWidth;
+changeTitle(size);
+
+window.addEventListener('resize', (event) => {
+  changeTitle(event.target.innerWidth, size);
+  size = event.target.innerWidth;
+});
